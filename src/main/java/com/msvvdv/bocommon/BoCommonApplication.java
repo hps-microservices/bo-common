1f2de8f022a75a4751261d167b4c6a364d72abb5
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2018-08-18 20:35:28 -0300

    Initial commit

---
+++ src/main/java/com/msvvdv/bocommon/BoCommonApplication.java
@@ -0,0 +1,12 @@
+package com.msvvdv.bocommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoCommonApplication.class, args);
+    }
+}
---
+++ src/test/java/com/msvvdv/bocommon/BoCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msvvdv.bocommon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoCommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
